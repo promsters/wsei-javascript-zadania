7ec32f919b1eb2761637a8cc80d336e7e15f366a
promsters <tomasz.ptakk@gmail.com>
2020-10-17 14:30:45 +0200

    Finished last chapter;

--- 15.DOM-Tworzenie elementów/script.js
+++ 15.DOM-Tworzenie elementów/script.js
@@ -100,35 +100,267 @@ document.querySelector('form#ex8').addEventListener('submit', function (event) {
 });
 
 // zadanie 9
-const ex9El = document.querySelector('#ex9');
-const ex9Form = ex9El.querySelector('form');
+const TableGenerator = {
+    /**
+     * @param {array} headers
+     * @param {array} records
+     * @param {Element} appendTo
+     */
+    generate: function(headers, records, appendTo) {
+        this.table = document.createElement('table');
 
-ex9El.querySelector('form > button').addEventListener('click', () => {
-    const prototype = ex9Form.querySelector('div:first-of-type').cloneNode(true);
+        this.addHeader(headers);
+        this.addBody(records);
 
-    prototype.querySelectorAll('input[type=text], input[type=number]').forEach((input) => {
-        input.value = null;
-        input.name = input.name.replace('[0]', `[${ex9Form.querySelectorAll('div').length}]`)
+        appendTo.appendChild(this.table);
+    },
+    /**
+     * @param {array} headers
+     */
+    addHeader: function (headers) {
+        if (headers.length === 0) {
+            return;
+        }
+
+        this.addRow(headers, 'th');
+    },
+    /**
+     * @param {array} records
+     */
+    addBody: function (records) {
+        records.forEach((record) => {
+            this.addRow(record, 'td');
+        });
+    },
+    /**
+     * @param {array} elements
+     * @param {string} elementTagName
+     */
+    addRow: function (elements, elementTagName) {
+        const tr = document.createElement('tr');
+        elements.forEach((value) => {
+            const element = document.createElement(elementTagName);
+            element.innerHTML = value;
+            tr.appendChild(element);
+        });
+
+        this.table.appendChild(tr);
+    }
+};
+
+const PeopleListFormHandler = {
+    init: function() {
+        this.container = document.querySelector('#ex9');
+        this.form = this.container.querySelector('form');
+        this.tableContainer = this.container.querySelector('#ex9-table-container');
+
+        this.form.querySelector('button').addEventListener('click', () => {
+            this.onAddMore();
+        });
+
+        this.form.addEventListener('submit', (event) => {
+            event.preventDefault();
+
+            this.onSubmit();
+        });
+
+        this.tableContainer.addEventListener('click', (event) => {
+            this.onTableClick(event.target);
+        });
+    },
+    onAddMore: function() {
+        const prototype = this.form.querySelector('div:first-of-type').cloneNode(true);
+
+        prototype.querySelectorAll('input[type=text], input[type=number]').forEach((input) => {
+            input.value = null;
+            input.name = input.name.replace('[0]', `[${this.form.querySelectorAll('div').length}]`)
+        });
+
+        this.form.querySelector('div:last-of-type').after(prototype);
+    },
+    onSubmit: function() {
+        const data = Object.fromEntries((new FormData(this.form)).entries());
+        let parsedData = [];
+        for(let i = 0;i < this.form.querySelectorAll('div').length;i++) {
+            let record = [
+                StringUtils.capitalizeFirst(data[`first_name[${i}]`]),
+                StringUtils.capitalizeFirst(data[`last_name[${i}]`]),
+                parseInt(data[`age[${i}]`]),
+                parseInt(data[`kids_amount[${i}]`]),
+                '<input type="button" class="remove-record" value="Usuń" />'
+            ];
+
+            parsedData.push(record);
+        }
+
+        if (parsedData.length > 0) {
+            this.generate(parsedData);
+        }
+    },
+    /**
+     * @param {Element} target
+     */
+    onTableClick: function (target) {
+        if (target.matches('.remove-record')) {
+            const table = target.closest('table');
+
+            target.closest('tr').remove();
+
+            if (table.querySelectorAll('td').length === 0) {
+                table.remove();
+            }
+        }
+    },
+    /**
+     * @param {array} data
+     */
+    generate: function(data) {
+        this.tableContainer.innerHTML = null;
+
+        TableGenerator.generate(['First name', 'Last name', 'Age', 'Kids amount'], data, this.tableContainer);
+    }
+};
+
+PeopleListFormHandler.init();
+
+// zadanie 10
+const StringUtils = {
+    /**
+     * @param {string} input
+     */
+    capitalizeFirst: function (input) {
+        if (typeof input !== "string" || input.length === 0) {
+            return input;
+        }
+
+        return input.charAt(0).toUpperCase() + input.slice(1);
+    }
+};
+
+// zadanie 11
+/**
+ * @param {string} input
+ */
+function numbersExtractor(input) {
+    const numbers = [...input.matchAll(/(\d+)/g)];
+
+    if (numbers.length === 0) {
+        return;
+    }
+    let sum = 0;
+    let product = 1;
+    numbers.forEach((number) => {
+        number = parseInt(number[0]);
+        sum += number;
+        product *= number;
     });
 
-    ex9Form.querySelector('div:last-of-type').after(prototype);
-});
+    console.log(sum);
 
-ex9Form.addEventListener('submit', (event) => {
-    event.preventDefault();
-
-    const data = Object.fromEntries((new FormData(ex9Form)).entries());
-    let parsedData = [];
-    for(let i = 0;i < ex9Form.querySelectorAll('div').length;i++) {
-        let object = {
-            firstName: data[`first_name[${i}]`],
-            lastName: data[`last_name[${i}]`],
-            age: parseInt(data[`age[${i}]`]),
-            kidsAmount: parseInt(data[`kids_amount[${i}]`])
-        };
-
-        parsedData.push(object);
+    for(let i = 0;i < product;i++) {
+        const div = document.createElement('div');
+        div.innerHTML = input;
+        document.body.appendChild(div);
     }
+}
 
-    console.log(parsedData);
-});
+numbersExtractor("asdasd");
+numbersExtractor("14xasd");
+numbersExtractor("1 3 axas 5");
+
+// zadanie 12
+/**
+ * @param {string} input
+ */
+function noIdeaForName(input) {
+    let object = {
+        text: input
+    };
+
+    object.replaceIfExists = function () {
+        if (this.text.includes('Ala')) {
+            this.text = this.text.replaceAll('Ala', 'Ola');
+            return;
+        }
+
+        const div = document.createElement('div');
+        div.innerHTML = 'Słowo Ala nie występuje w tekście';
+        document.body.append(div);
+        return false;
+    };
+
+    object.replaceIfExists();
+    return object;
+}
+
+// zadanie 13
+/**
+ * @param {string[]} stringArray
+ */
+function countLetters(stringArray) {
+    let result = [];
+    stringArray.forEach((input) => {
+        result.push([...input.matchAll(/[A-z]/g)].length);
+    });
+
+    return result;
+}
+
+console.log(countLetters(["test", "he123", "dupa"]));
+
+/**
+ * @param {int[]} numberArray
+ */
+function sumNumbers(numberArray) {
+    let sum = 0;
+    numberArray.forEach((number) => {
+        sum += number;
+    });
+
+    return sum;
+}
+
+console.log(sumNumbers([15, 24, 10]));
+
+/**
+ * @param {int[]} numberArray
+ */
+function avgNumbers(numberArray) {
+    return sumNumbers(numberArray)/numberArray.length;
+}
+
+console.log(avgNumbers([15, 24, 10]));
+
+// zadanie 14
+let object14 = {
+    name: '',
+    surname: '',
+    age: ''
+};
+
+const modifyObject = (name, surname, age) => {
+    object14.name = name;
+    object14.nameLength = object14.name.length;
+    object14.surname = surname;
+    object14.surnameLength = object14.surname.length;
+    object14.age = age;
+    object14.ageLength = object14.age.length;
+
+    if (object14.nameLength > 5 || object14.surnameLength > 5 || object14.ageLength > 5) {
+        const button = document.createElement('button');
+        button.innerHTML = 'Reset';
+
+        button.addEventListener('click', () => {
+            object14 = {
+                name: '',
+                surname: '',
+                age: ''
+            };
+            button.remove();
+        });
+
+        document.body.append(button);
+    }
+};
+
+modifyObject("Tomasz", "Ptak", "25");
